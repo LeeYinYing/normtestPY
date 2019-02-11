404d6f2c3db72b16ac5e83facc1047bacd4fd0fc
Sylvia Lee <sylvialee19@hotmail.com>
2019-02-10 16:15:10 -0800

    Written params_mle function + modified setup.py to indicate dependencies + minor changes to documentation style for consistency

--- normtestPY/params_mle.py
+++ normtestPY/params_mle.py
@@ -1,21 +1,66 @@
+import pandas as pd
+import numpy as np
+
 def params_mle(data):
     '''
     Fit data to a Guassian distribution with Maximum Likelihood Estimation (MLE)
 
     Parameters
     ----------
-    DATAFRAME where data for each continous variable is in its
-              respective column
+    data : ndarray, list, dict, or DataFrame
+        Data to be tested for normality
 
     Returns
     -------
-    DATAFRAME where the first row contains the estimated means and
-              the second row contains the estimated variance, and the columns
-              present the original variables in the data
+    mle_params : Dataframe
+        Dataframe with the first row containing the estimated means and
+        the second row containing the estimated variance. The columns
+        present the original variables in the data
 
     Examples
     --------
+    iris_data = pd.DataFrame({"length": [1,2,3,4], "width": [5,6,7,8])
+    make_qqplot(iris_data)
     '''
-    print("params_mle - empty function")
 
-    return()
+
+    ## PREPROCESSING
+    ## =============
+
+    # Set default column names as indexes
+    try: # 2D data
+        n_var = data.shape[1]
+        var_names = range(n_var)
+    except: # 1D data
+        var_names = [0]
+
+    # Address different input types
+    if isinstance(data, pd.DataFrame):
+        var_names = list(data)
+        data = np.array(data)
+
+    elif isinstance(data, pd.Series):
+        if data.name != None:
+            var_names = [data.name]
+        data = np.array(data)
+
+    elif isinstance(data, list):
+        var_names = range(len(data))
+        data = np.transpose(np.array(data))
+
+    n_obs = data.shape[0]
+
+    ## Calculations
+    ## =============
+
+    # Calculate mu estimates
+    mu = np.sum(np.array(data), axis = 0)/n_obs
+
+    # Calculate sigma estimates
+    variance = np.sum((data - mu)**2, axis = 0)/n_obs
+    sigma = variance**(1/2)
+
+    ## Return results
+    ## ==============
+    mle_params = pd.DataFrame(np.vstack((mu, variance)), index = ["Mean", "Variance"], columns = var_names)
+    return(mle_params)
